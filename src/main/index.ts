6dd545d8789ff7be2a024b69b7051fc14b664211
Marcos Reis <marcosreisdossantos01@gmail.com>
2022-12-26 18:26:38 -0300

    refactor: add health endpoint

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,4 +2,5 @@ import express from "express"
 const app = express()
 const PORT = 3000
 app.get("/",(_,res)=>res.send("Hello World!"))
+app.get("/health", (_,res)=>res.sendStatus(200))
 app.listen( PORT , () => { console.log(`Listening on port ${ PORT }`) })
