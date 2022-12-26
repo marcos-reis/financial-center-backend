75012e511885e625afbd8480b263b1b2ce009422
Marcos Reis <marcosreisdossantos01@gmail.com>
2022-12-26 16:05:25 -0300

    refactor: fix index file

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,5 @@
 import express from "express"
-const  app = express()
+const app = express()
 const PORT = 3000
-app.use((_,res)=>res.send("Hello World!"))
-app.listen( PORT , () => {
-    console.log(`Listening on port ${ PORT }`)
-})
+app.get("/",(_,res)=>res.send("Hello World!"))
+app.listen( PORT , () => { console.log(`Listening on port ${ PORT }`) })
